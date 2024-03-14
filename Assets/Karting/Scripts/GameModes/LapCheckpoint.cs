e0006dff6373eb78e2398728fc46089c75954478
anthonytremblay3 <anthony.tremblay.3@ens.etsmtl.ca>
2024-03-14 11:22:32 -0400

    un peu de progres

---
+++ Assets/Karting/Scripts/GameModes/LapCheckpoint.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// This class inherits from TargetObject and represents a PickupObject.
+/// </summary>
+public class LapCheckpoint : TargetObject
+{
+    [HideInInspector]
+    public bool isTriggered;
+    
+    void Start() {
+        Register();
+    }
+
+    void OnCollect()
+    {
+        Objective.OnRegisterPickup(this);
+        this.active = false;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if ((layerMask.value & 1 << other.gameObject.layer) > 0 && other.gameObject.CompareTag("Player"))
+        {
+            OnCollect();
+        }
+    }
+}
