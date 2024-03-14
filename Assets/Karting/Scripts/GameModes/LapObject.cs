1c9e1360b33084a6141a59dea95bc632cc15b60f
anthonytremblay3 <anthony.tremblay.3@ens.etsmtl.ca>
2024-03-14 15:15:25 -0400

    Systeme de lap avec checkpoint fonctione

--- Assets/Karting/Scripts/GameModes/LapObject.cs
+++ Assets/Karting/Scripts/GameModes/LapObject.cs
@@ -9,8 +9,16 @@ public class LapObject : TargetObject
     [Tooltip("Is this the first/last lap object?")]
     public bool finishLap;
 
+    [Tooltip("Sound of second lap")]
+    public AudioClip CollectSound1;
+
+    [Tooltip("Sound of last lap")]
+    public AudioClip CollectSound2;
+
     [HideInInspector]
     public bool lapOverNextPass;
+
+    private int currentLap = 0;
 
     void Start() {
         Register();
@@ -25,7 +33,21 @@ public class LapObject : TargetObject
     {
         if (!((layerMask.value & 1 << other.gameObject.layer) > 0 && other.CompareTag("Player")))
             return;
-       
+
         Objective.OnUnregisterPickup?.Invoke(this);
+
+        switch(currentLap) {
+            case 0: AudioUtility.CreateSFX(CollectSound, transform.position, AudioUtility.AudioGroups.Pickup, 0f);
+                break;
+            
+            case 1: AudioUtility.CreateSFX(CollectSound1, transform.position, AudioUtility.AudioGroups.Pickup, 0f);
+                break;
+
+            case 2: AudioUtility.CreateSFX(CollectSound2, transform.position, AudioUtility.AudioGroups.Pickup, 0f);
+                break;
+            default:
+                break;
+        }
+        currentLap++;
     }
 }
